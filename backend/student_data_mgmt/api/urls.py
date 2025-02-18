4cea4cf439ab5e14320ddd2f5d0b7fa5757ad789
sanjivthapa <thapasvt12@gmail.com>
2025-02-18 13:30:36 +0545

    Initialized Rest framework on backend

--- backend/student_data_mgmt/api/urls.py
+++ backend/student_data_mgmt/api/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import routers
 from . import views
 
+
 urlpatterns = [
-    
-    # Add more URL patterns here
+    path('student/', views.StudentListCreate.as_view(), name='student-view-create'),
+    path('student/<int:id>/', views.StudentRetrieveUpdateDestroy.as_view(), name='student-view-retrieve-update-destroy'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
