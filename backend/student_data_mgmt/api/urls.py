d301e99a12468f35e8a7e264f68b603dc3507a14
sanjivthapa <thapasvt12@gmail.com>
2025-02-20 14:08:08 +0545

    Used sqlite database for now will change this later

--- backend/student_data_mgmt/api/urls.py
+++ backend/student_data_mgmt/api/urls.py
@@ -1,14 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework import routers
 from . import views
 
 
 urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name='register'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
     # path('students/', views.StudentListCreate.as_view(), name='student-view-create'),
     # path('students/delete/<int:id>/', views.StudentDelete.as_view(), name='delete-student'),
 
--- backend/student_data_mgmt/student_data_mgmt/urls.py
+++ backend/student_data_mgmt/student_data_mgmt/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from api.views import RegisterView, LoginView, LogoutView
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('api/user/register/', CreateUserView.as_view(), name='register'),
-    # path('api/token/', TokenObtainPairView.as_view(), name='get-token'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh-token'),
-    # path('api-auth/', include('rest_framework.urls')),
+    path('admin/', admin.site.urls),
+    path('api/auth/register/', RegisterView.as_view(), name='register'),
+    path('api/auth/login/', LoginView.as_view(), name='login'),
+    path('api/auth/logout/', LogoutView.as_view(), name='logout'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('api.urls')),
 ]
