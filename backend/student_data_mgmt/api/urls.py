f6f5406f011b74d6f81bd1f84c72f279a4bf4f2a
sanjivthapa <thapasvt12@gmail.com>
2025-02-23 20:46:07 +0545

    Backend API is working fine tested it, GET, POST, PUT, DELETE is working for individual it has relation with student and you can also view marks of all student

--- backend/student_data_mgmt/api/urls.py
+++ backend/student_data_mgmt/api/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+from .views import StudentDetail, StudentCreate, StudentMarksAll, StudentsMarks 
 
 urlpatterns = [
-    # URl for student maangement
-    path('students/', views.StudentCreate.as_view(), name='student-create'), 
-    path('students/<int:id>/', views.StudentDetail.as_view(), name='student-detail'),
-    path('students/marks/', views.StudentsMarks.as_view(), name='students-marks'),
-    path('students/marks/<int:id>/', views.StudentMarksDetail.as_view(), name='students-marks-detail'),
+    path('students/<int:id>/', StudentDetail.as_view(), name='student-detail'),
+    path('students/', StudentCreate.as_view(), name='student-create'), 
+    path('students/marks/', StudentMarksAll.as_view(), name='student-marks-all'),
+    path('students/marks/<int:id>/', StudentsMarks.as_view(), name='student-marks-detail'),
 ]
+
 # For media file i.e images
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
