5b52fcb94ed116c040bd36433bf6eb9e8f6ce4e4
sanjivthapa <thapasvt12@gmail.com>
2025-02-23 19:05:32 +0545

    Created Backend API for students marks and they are working fine in admin panel just need to impleament them in Frontend

--- backend/student_data_mgmt/api/admin.py
+++ backend/student_data_mgmt/api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, StudentMarks
 # Register your models here.
 
 class studentAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'roll', 'address', 'student_class','section', 'photo']
     
 admin.site.register(Student, studentAdmin)
+admin.site.register(StudentMarks)
